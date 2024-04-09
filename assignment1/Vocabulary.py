ef697fda1bd3e11997fbc2a61f68b7e4b7d61db6
Zexian <zexianlie@gmail.com>
2024-04-09 14:56:22 -0700

    Change tabs to spaces

--- assignment1/Vocabulary.py
+++ assignment1/Vocabulary.py
@@ -2,84 +2,86 @@ from collections import Counter
 from re import sub, compile
 import matplotlib.pyplot as plt
 import numpy as np
+from nltk.tokenize import RegexpTokenizer
 
 class UnimplementedFunctionError(Exception):
-	pass
+    pass
 
 class Vocabulary:
 
-	def __init__(self, corpus):
+    def __init__(self, corpus):
 
-		self.word2idx, self.idx2word, self.freq = self.build_vocab(corpus)
-		self.size = len(self.word2idx)
+        self.word2idx, self.idx2word, self.freq = self.build_vocab(corpus)
+        self.size = len(self.word2idx)
 
-	def most_common(self, k):
-		freq = sorted(self.freq.items(), key=lambda x: x[1], reverse=True)
-		return [t for t,f in freq[:k]]
-
-
-	def text2idx(self, text):
-		tokens = self.tokenize(text)
-		return [self.word2idx[t] if t in self.word2idx.keys() else self.word2idx['UNK'] for t in tokens]
-
-	def idx2text(self, idxs):
-		return [self.idx2word[i] if i in self.idx2word.keys() else 'UNK' for i in idxs]
+    def most_common(self, k):
+        freq = sorted(self.freq.items(), key=lambda x: x[1], reverse=True)
+        return [t for t,f in freq[:k]]
 
 
-	###########################
-	## TASK 1.1           	 ##
-	###########################
-	def tokenize(self, text):
-		"""
-	    
-	    tokenize takes in a string of text and returns an array of strings splitting the text into discrete tokens.
+    def text2idx(self, text):
+        tokens = self.tokenize(text)
+        return [self.word2idx[t] if t in self.word2idx.keys() else self.word2idx['UNK'] for t in tokens]
 
-	    :params: 
-	    - text: a string to be tokenize, e.g. "The blue dog jumped, but not high."
-
-	    :returns:
-	    - tokens: a list of strings derived from the text, e.g. ["the", "blue", "dog", "jumped", "but", "not", "high"] for word-level tokenization
-	    
-	    """ 
-
-		# REMOVE THIS ONCE YOU IMPLEMENT THIS FUNCTION
-		raise UnimplementedFunctionError("You have not yet implemented tokenize.")
+    def idx2text(self, idxs):
+        return [self.idx2word[i] if i in self.idx2word.keys() else 'UNK' for i in idxs]
 
 
+    ###########################
+    ## TASK 1.1           	 ##
+    ###########################
+    def tokenize(self, text):
+        """
+        
+        tokenize takes in a string of text and returns an array of strings splitting the text into discrete tokens.
 
-	###########################
-	## TASK 1.2            	 ##
-	###########################
-	def build_vocab(self,corpus):
-		"""
-	    
-	    build_vocab takes in list of strings corresponding to a text corpus, tokenizes the strings, and builds a finite vocabulary
+        :params: 
+        - text: a string to be tokenize, e.g. "The blue dog jumped, but not high."
 
-	    :params:
-	    - corpus: a list string to build a vocabulary over
+        :returns:
+        - tokens: a list of strings derived from the text, e.g. ["the", "blue", "dog", "jumped", "but", "not", "high"] for word-level tokenization
+        
+        """ 
 
-	    :returns: 
-	    - word2idx: a dictionary mapping token strings to their numerical index in the dictionary e.g. { "dog": 0, "but":1, ..., "UNK":129}
-	    - idx2word: the inverse of word2idx mapping an index in the vocabulary to its word e.g. {0: "dog", 1:"but", ..., 129:"UNK"}
-	    - freq: a dictionary of words and frequency counts over the corpus (including words not in the dictionary), e.g. {"dog":102, "the": 18023, ...}
-
-	    """ 
-
-		# REMOVE THIS ONCE YOU IMPLEMENT THIS FUNCTION
-		raise UnimplementedFunctionError("You have not yet implemented build_vocab.")
+        # REMOVE THIS ONCE YOU IMPLEMENT THIS FUNCTION
+        tokenizer = RegexpTokenizer(r'\w+')
+        tokens = tokenizer.tokenize(text)
+        return tokens
 
 
-	###########################
-	## TASK 1.3              ##
-	###########################
-	def make_vocab_charts(self):
-		"""
-	    
-	    make_vocab_charts plots word frequency and cumulative coverage charts for this vocabulary. See handout for more details
+    ###########################
+    ## TASK 1.2            	 ##
+    ###########################
+    def build_vocab(self,corpus):
+        """
+        
+        build_vocab takes in list of strings corresponding to a text corpus, tokenizes the strings, and builds a finite vocabulary
 
-	    
-	    """ 
+        :params:
+        - corpus: a list string to build a vocabulary over
 
-	    # REMOVE THIS ONCE YOU IMPLEMENT THIS FUNCTION
-		raise UnimplementedFunctionError("You have not yet implemented make_vocab_charts.")
+        :returns: 
+        - word2idx: a dictionary mapping token strings to their numerical index in the dictionary e.g. { "dog": 0, "but":1, ..., "UNK":129}
+        - idx2word: the inverse of word2idx mapping an index in the vocabulary to its word e.g. {0: "dog", 1:"but", ..., 129:"UNK"}
+        - freq: a dictionary of words and frequency counts over the corpus (including words not in the dictionary), e.g. {"dog":102, "the": 18023, ...}
+
+        """ 
+
+        # REMOVE THIS ONCE YOU IMPLEMENT THIS FUNCTION
+        raise UnimplementedFunctionError("You have not yet implemented build_vocab.")
+
+
+    ###########################
+    ## TASK 1.3              ##
+    ###########################
+    def make_vocab_charts(self):
+        """
+        
+        make_vocab_charts plots word frequency and cumulative coverage charts for this vocabulary. See handout for more details
+
+        
+        """ 
+
+        # REMOVE THIS ONCE YOU IMPLEMENT THIS FUNCTION
+        raise UnimplementedFunctionError("You have not yet implemented make_vocab_charts.")
 
